5745664759f5328145d2ab904b4db877cca46f96
abdelaatife <abdoabdo2002104h@gmail.com>
2022-06-06 13:19:54 +0100

    date page 2

--- lib/screens/calendre.dart
+++ lib/screens/calendre.dart
@@ -1,6 +1,8 @@
 import 'package:ddialyse/screens/const.dart';
 import 'package:flutter/material.dart';
-import 'package:calendar_agenda/calendar_agenda.dart';
+import 'package:flutter_clean_calendar/clean_calendar_event.dart';
+import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
+import 'package:ionicons/ionicons.dart';
 
 class calendre extends StatefulWidget {
   const calendre({Key? key}) : super(key: key);
@@ -10,30 +12,62 @@ class calendre extends StatefulWidget {
 }
 
 class _calendreState extends State<calendre> {
-  List a = [];
+  @override
+  final Map<DateTime, List<CleanCalendarEvent>> _events = {
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day): [
+      CleanCalendarEvent('mission',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 12, 0),
+          description: 'measure your renal function',
+          color: Colors.blue),
+    ],
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _handleNewDate(DateTime(
+        DateTime.now().year, DateTime.now().month, DateTime.now().day));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primary,
+        elevation: 2,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: primary,
+          child: Icon(
+            Ionicons.add_outline,
+            size: 35,
+          )),
       body: SafeArea(
-        child: Container(
-          child: CalendarAgenda(
-            backgroundColor: primary,
-            weekDay: WeekDay.long,
-            calendarEventColor: primary,
-            fullCalendarScroll: FullCalendarScroll.horizontal,
-            fullCalendarDay: WeekDay.long,
-            selectedDateColor: bleu,
-            initialDate: DateTime.now(),
-            firstDate: DateTime.now().subtract(Duration(days: 140)),
-            lastDate: DateTime.now().add(Duration(days: 380)),
-            events: List.generate(
-                100, (index) => DateTime.now().add(Duration(days: 1))),
-            onDateSelected: (date) {},
-            calendarLogo: Image.asset("asset/logo.png"),
-            selectedDayLogo: AssetImage("asset/logo.png"),
-          ),
+        child: Calendar(
+          todayColor: primary,
+          startOnMonday: true,
+          weekDays: ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
+          events: _events,
+          isExpandable: true,
+          eventDoneColor: Colors.green,
+          selectedColor: primary,
+          eventColor: bleu,
+          locale: 'en',
+          isExpanded: true,
+          expandableDateFormat: 'EEEE, dd. MMMM yyyy',
+          dayOfWeekStyle: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w800, fontSize: 11),
         ),
       ),
     );
+  }
+
+  void _handleNewDate(date) {
+    print('Date selected: $date');
   }
 }
